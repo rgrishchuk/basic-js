c63558abed4df9ebc4c12b45e9984ec2becddb59
Ruslan Hryshchuk <rus_g@tut.by>
2020-09-26 00:00:12 +0300

    feat: solved vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,74 @@
 const CustomError = require("../extensions/custom-error");
-
+function isLetter(charCode) {
+	return charCode >= 65 && charCode <= 90;
+}
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
+  constructor(direct = true) {
+		this.direct = direct;
+	}
+  encrypt(message, key) {
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
+    if (arguments.length < 2) throw new Error();
+		let result = '';
+		let messageText = message.toUpperCase();
+		let keyText = '';
+		let j = 0;
+		for (let i = 0; i < message.length; i++) {
+			if ( isLetter(messageText.charCodeAt(i)) ) {
+				if (j < key.length) {
+					keyText += key.charAt(j).toUpperCase();
+					j++;
+				} else {
+					keyText += key.charAt(0).toUpperCase();
+					j = 1;
+				}
+			} else {	
+				keyText += messageText.charAt(i);
+			}
+		}
+		for (let i = 0; i< message.length; i++) {
+			if (isLetter(messageText.charCodeAt(i)) ) {
+				result = result + String.fromCharCode(65 + (messageText.charCodeAt(i) + keyText.charCodeAt(i)) % 26);
+			} else {
+				result = result + messageText.charAt(i);			
+			}
+		}
+
+		if (this.direct) return result
+		else return result.split("").reverse().join(""); 
+}    
+  decrypt(message, key) {
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
+    if (arguments.length < 2) throw new Error();
+		let result = '';
+		let keyText = '';
+		let messageText = message.toUpperCase();
+		let j = 0;
+		for (let i = 0; i < message.length; i++) {
+			if ( isLetter(messageText.charCodeAt(i)) ) {
+				if (j < key.length) {
+					keyText += key.charAt(j).toUpperCase();
+					j++;
+				} else {
+					keyText += key.charAt(0).toUpperCase();
+					j = 1;
+				}
+			} else {	
+				keyText += messageText.charAt(i);
+			}
+		}
+		for (let i = 0; i< message.length; i++) {
+			if (isLetter(messageText.charCodeAt(i)) ) {
+				result = result + String.fromCharCode(65 + (messageText.charCodeAt(i) + 26 - keyText.charCodeAt(i)) % 26);
+			} else {
+				result = result + messageText.charAt(i);			
+			}
+		}
+
+		if (this.direct) return result
+		else return result.split("").reverse().join(""); 
   }
 }
 
